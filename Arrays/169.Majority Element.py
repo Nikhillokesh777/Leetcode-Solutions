ae21763e4da4a0da1cf8c40e937970425c455c5f
Nikhillokesh777 <nikhillokesh53@gmail.com>
2025-06-28 22:25:16 +0530

    Add Majority Element solution

---
+++ Arrays/169.Majority Element.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        temp = len(nums)//2
+        return nums[temp]
+             
